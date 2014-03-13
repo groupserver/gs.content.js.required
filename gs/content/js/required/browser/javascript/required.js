5e560ac51d3bfffb06fd5c4c8a1b8b5af77f9797
Michael JasonSmith <mpj17@onlinegroups.net>
2014-03-13 11:11:52 -0700

    Minor whitespace changes, trying to track down an IE8 error.

--- gs/content/js/required/browser/javascript/required.js
+++ gs/content/js/required/browser/javascript/required.js
@@ -1,3 +1,13 @@
+// Copyright © 2013, 2014 OnlineGroups.net and Contributors.
+// All Rights Reserved.
+//
+// This software is subject to the provisions of the Zope Public License,
+// Version 2.1 (ZPL). http://groupserver.org/downloads/license/
+//
+// THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+// WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND
+// FITNESS FOR A PARTICULAR PURPOSE.
 jQuery.noConflict();
 
 function GSContentRequiredInterlock (form, button) {
@@ -52,7 +62,7 @@ function GSContentRequiredInterlock (form, button) {
     return {
         required_widgets: function () {return requiredWidgets;},
         unfinished_widgets: function () {return unfinishedWidgets;}
-    }
+    };
 } // GSContentRequiredInterlock
 
 function GSContentRequired (formId, buttonId) {
@@ -72,7 +82,7 @@ function GSContentRequired (formId, buttonId) {
 } // GSContentRequired
 
 jQuery(window).load(function () {
-    var form = null, buttonId = null, button = null, required = null;
+    var form=null, buttonId=null, button=null, required=null;
 
     form = jQuery('form.gs-content-js-required');
     if ( form.is('*') ) {
